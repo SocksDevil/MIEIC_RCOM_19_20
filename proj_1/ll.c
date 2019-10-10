81da32b3c7d86b5882240452134db1d764a2d9ab
Andre Rocha <andrefmrocha@live.com.pt>
2019-10-10 18:07:04 +0100

    Updated state machine and added read data

--- proj_1/ll.c
+++ proj_1/ll.c
@@ -22,14 +22,14 @@ int llopen(int port, connection_role role) {
   return fd;
 }
 
-int llwrite(int fd, char * buffer, int length) {
+// int llwrite(int fd, char * buffer, int length) {
 
-    // stuff buffer
+//     // stuff buffer
 
-    // send buffer
+//     // send buffer
   
-  return fd;
-}
+//   return fd;
+// }
 
 int llclose(int fd) {
     return close_connection(fd);
