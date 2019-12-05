257eb9debc8a77539f9811a932e1f215153bcd87
Andre Rocha <andrefmrocha@live.com.pt>
2019-12-05 12:35:58 +0000

    Finished bash scripts

--- config/config_tux1.bash
+++ config/config_tux1.bash
@@ -1,4 +1,5 @@
 ifconfig eth0 up
-ifconfig eth0 172.16.50.1/24
-route add -net 172.16.51.1/24 gw 172.16.50.254
-
+ifconfig eth0 172.16.30.1/24
+route add -net 172.16.31.0/24 gw 172.16.30.254
+route add default gw 172.16.30.254
+echo "nameserver 172.16.1.1" > /etc/resolv.conf
